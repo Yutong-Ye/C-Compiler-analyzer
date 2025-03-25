0e74c4181c073c8ef4168c5b4b730cf1dec7b785
Yutong-Ye <yye103@gmail.com>
2025-03-25 11:44:53 -0400

    init

---
+++ ParseTree1.cpp
@@ -0,0 +1,27 @@
+sum + (47 / total)
+Next token is: 11, Next lexeme is sum
+1 Enter <expr>   
+2 Enter <term>
+3 Enter <factor>
+Next token is: 21, Next lexeme is +
+3 Exit <factor>
+2 Exit <term>
+Next token is: 25, Next lexeme is (
+4 Enter <term>
+5 Enter <factor>
+Next token is: 10, Next lexeme is 47
+6 Enter <expr>
+7 Enter <term>
+8 Enter <factor>
+Next token is: 24, Next lexeme is /
+8 Exit <factor>
+Next token is: 11, Next lexeme is total
+9 Enter <factor>
+Next token is: 26, Next lexeme is )
+9 Exit <factor>
+7 Exit <term>
+6 Exit <expr> 
+Next token is: -1, Next lexeme is EOF
+5 Exit <factor>
+4 Exit <term>
+1 Exit <expr> 
---
+++ ParseTree2.cpp
@@ -0,0 +1,59 @@
+(sum + 47) * (b * b - 4 * a * c) / total
+
+
+Next token is: 25, Next lexeme is (
+1 Enter <expr>
+2 Enter <term>
+3 Enter <factor>
+Next token is: 11, Next lexeme is sum
+4 Enter <expr>
+5 Enter <term>
+6 Enter <factor>
+Next token is: 21, Next lexeme is +
+6 Exit <factor>
+5 Exit <term>
+Next token is: 10, Next lexeme is 47
+7 Enter <term>
+8 Enter <factor>
+Next token is: 26, Next lexeme is )
+8 Exit <factor>
+7 Exit <term>
+4 Exit <expr> 
+Next token is: 23, Next lexeme is *
+3 Exit <factor>
+Next token is: 25, Next lexeme is (
+9 Enter <factor>
+Next token is: 11, Next lexeme is b
+10 Enter <expr>
+11 Enter <term>
+12 Enter <factor>
+Next token is: 23, Next lexeme is *
+12 Exit <factor>
+Next token is: 11, Next lexeme is b
+13 Enter <factor>
+Next token is: 22, Next lexeme is -
+13 Exit <factor>
+11 Exit <term>
+Next token is: 10, Next lexeme is 4
+14 Enter <term>
+15 Enter <factor>
+Next token is: 23, Next lexeme is *
+15 Exit <factor>
+Next token is: 11, Next lexeme is a
+16 Enter <factor>
+Next token is: 23, Next lexeme is *
+16 Exit <factor>
+Next token is: 11, Next lexeme is c
+17 Enter <factor>
+Next token is: 26, Next lexeme is )
+17 Exit <factor>
+14 Exit <term>
+10 Exit <expr> 
+Next token is: 24, Next lexeme is /
+9 Exit <factor>
+Next token is: 11, Next lexeme is total
+18 Enter <factor>
+Next token is: -1, Next lexeme is EOF
+18 Exit <factor>
+2 Exit <term>
+1 Exit <expr> 
